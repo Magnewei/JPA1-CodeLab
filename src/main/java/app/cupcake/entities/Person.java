55152010c9e26be01bde318a81b93280906f640e
Anton Stengaard <afstengaard@gmail.com>
2024-08-28 09:25:28 +0200

    Person klasse

---
+++ src/main/java/app/cupcake/entities/Person.java
@@ -0,0 +1,30 @@
+package app.cupcake.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+/**
+ * Purpose:
+ *
+ * @Author: Anton Friis Stengaard
+ */
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class Person {
+    @Id
+    @Column(name = "id",unique = true,nullable = false)
+    private int id;
+    @Column(name = "name",unique = false, nullable = false)
+    private String name;
+    @Column(name = "age",unique = false,nullable = false)
+    private int age;
+}
