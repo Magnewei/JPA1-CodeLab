4f64552ae2267bd69b46451a7f695ffcb1eb5d30
Magnewei <magnewei@icloud.com>
2024-08-28 10:09:07 +0200

    Fixed DAO interface

---
+++ src/test/java/app/cupcake/persistence/daos/PersonDAOTest.java
@@ -0,0 +1,25 @@
+package app.cupcake.persistence.daos;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonDAOTest {
+
+    @Test
+    void create() {
+
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void update() {
+    }
+}
